f607dbc1bd3563a70a12491bfb740939738e2675
LEGION\Martin <martiniliev1311@gmail.com>
2025-11-24 21:05:26 +0200

    chore: simplify Docker setup

--- apps/backend/start.sh
+++ apps/backend/start.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+# #!/bin/sh
 
-echo "Running database migrations..."
-npx medusa db:migrate
+# echo "Running database migrations..."
+# npx medusa db:migrate
 
-echo "Seeding database..."
-npm run seed || echo "Seeding failed, continuing..."
+# echo "Seeding database..."
+# npm run seed || echo "Seeding failed, continuing..."
 
-echo "Starting Medusa production server..."
-npm start
+# echo "Starting Medusa production server..."
+# npm start
