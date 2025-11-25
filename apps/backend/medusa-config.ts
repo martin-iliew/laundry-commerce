0aba8acdb3a88648457185947d0c9410018301dd
LEGION\Martin <martiniliev1311@gmail.com>
2025-11-25 18:26:01 +0200

    feat: edit Medusa configuration file to add redis

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -48,5 +48,26 @@ module.exports = defineConfig({
               ],
       },
     },
+    {
+      resolve: "@medusajs/medusa/caching",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/caching-redis",
+            id: "caching-redis",
+            is_default: true,
+            options: {
+              redisUrl: process.env.CACHE_REDIS_URL,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.EVENTS_REDIS_URL,
+      },
+    },
   ],
 });
